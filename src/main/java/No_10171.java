f566f1bbbdc78cdfc347a72e89b0e7652c625755
sha11 <sha1119@naver.com>
2021-08-16 15:23:26 +0900

    No_10171 commit

---
+++ src/main/java/No_10171.java
@@ -0,0 +1,9 @@
+public class No_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
