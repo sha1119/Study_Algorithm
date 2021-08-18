4f4457965e2df508ec77284111cc3b5acc6cdc7e
sha11 <sha1119@naver.com>
2021-08-18 22:04:59 +0900

    No_10950 commit

---
+++ src/main/java/Step_3/No_10950.java
@@ -0,0 +1,15 @@
+package Step_3;
+
+import java.util.Scanner;
+
+public class No_10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
