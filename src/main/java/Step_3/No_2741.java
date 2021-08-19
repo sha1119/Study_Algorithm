527dbb5cab075fbdb9cad9216a97a03016083d8f
sha11 <sha1119@naver.com>
2021-08-19 21:27:29 +0900

    No_2741 commit

---
+++ src/main/java/Step_3/No_2741.java
@@ -0,0 +1,13 @@
+package Step_3;
+
+import java.util.Scanner;
+
+public class No_2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        for (int a = 1; a <= i; a++) {
+            System.out.println(a);
+        }
+    }
+}
