587f2c6301fa171992b4c071e164e69885d86b84
sha11 <sha1119@naver.com>
2021-08-23 21:17:46 +0900

    No_1110 commit

---
+++ src/main/java/Step_4/No_1110.java
@@ -0,0 +1,23 @@
+package Step_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int raw = 0;
+        int copy = N;
+
+        do {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            raw++;
+        } while (copy != N);
+
+        System.out.println(raw);
+    }
+}
