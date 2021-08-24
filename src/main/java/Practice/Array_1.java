b4357bd0e7fc28f3bc4622915c70ac041086c749
sha11 <sha1119@naver.com>
2021-08-24 22:13:24 +0900

    Array_1 commit

---
+++ src/main/java/Practice/Array_1.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import java.util.Arrays;
+
+public class Array_1 {
+    public static void main(String[] args) {
+        int[] iArr = {80, 70, 60, 95, 100};
+        for (int i = 0; i < iArr.length; i++)
+            System.out.println(iArr[i]);
+        System.out.println(Arrays.toString(iArr));
+    }
+}
