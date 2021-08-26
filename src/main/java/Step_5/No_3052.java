2c2108ce4b2974c5e8391ead79f729acd015cd9f
sha11 <sha1119@naver.com>
2021-08-26 22:57:03 +0900

    No_3052 commit

---
+++ src/main/java/Step_5/No_3052.java
@@ -0,0 +1,24 @@
+package Step_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] arr = new boolean[42];
+
+        for(int i = 0; i < 10; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
+        }
+
+        int count = 0;
+        for (boolean value : arr) {
+            if (value) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
