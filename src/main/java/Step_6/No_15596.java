9613ae416e610671cccabd3cb4166805113f68f2
sha11 <sha1119@naver.com>
2021-08-27 14:08:16 +0900

    No_15596 commit

---
+++ src/main/java/Step_6/No_15596.java
@@ -0,0 +1,12 @@
+package Step_6;
+
+class No_15596 {
+    long sum(int[] a) {
+        long sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
