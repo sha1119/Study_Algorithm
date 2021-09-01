a13871a884cb0e37085f6faf6be8f415f7f50f79
sha11 <sha1119@naver.com>
2021-09-01 20:14:28 +0900

    No_11654 commit

---
+++ src/main/java/Step_7/No_11654.java
@@ -0,0 +1,16 @@
+package Step_7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_11654 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] a = br.readLine().toCharArray();
+
+        System.out.print((int)a[0]);
+    }
+}
