5f3d567d20d54e28a4c6c4658007100c12bfc335
sha11 <sha1119@naver.com>
2021-09-05 21:58:57 +0900

    No_2908 commit

---
+++ src/main/java/Step_7/No_2908.java
@@ -0,0 +1,18 @@
+package Step_7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.print(a > b ? a : b);
+
+    }
+}
