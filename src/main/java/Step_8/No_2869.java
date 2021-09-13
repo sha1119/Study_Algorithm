8fd74677e8ab0fb7805f28a6395eae812493da2c
sha11 <sha1119@naver.com>
2021-09-13 20:55:05 +0900

    No_2869 commit

---
+++ src/main/java/Step_8/No_2869.java
@@ -0,0 +1,23 @@
+package Step_8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int climb = (V - B) / (A - B);
+
+        if ((V - B) % (A - B) != 0) {
+            climb++;
+        }
+        System.out.println(climb);
+    }
+}
