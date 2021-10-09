0f43d116c959b17cdf306a456165b4291ad6a98a
sha11 <sha1119@naver.com>
2021-10-09 20:35:33 +0900

    No_17413 commit

---
+++ src/main/java/DataStructure/No_17413.java
@@ -0,0 +1,47 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class No_17413 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = bufferedReader.readLine();
+        StringBuilder result = new StringBuilder();
+        boolean check = false;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '<') {
+                check = true;
+                while (!stack.isEmpty()) {
+                    result.append(stack.pop());
+                }
+                result.append(input.charAt(i));
+            } else if (input.charAt(i) == '>') {
+                check = false;
+                result.append(input.charAt(i));
+            } else if (input.charAt(i) == ' ') {
+                while (!stack.isEmpty()) {
+                    result.append(stack.pop());
+                }
+                result.append(input.charAt(i));
+            } else {
+                if (check == true) {
+                    result.append(input.charAt(i));
+                } else {
+                    stack.push(input.charAt(i));
+                }
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        System.out.println(result);
+    }
+}
