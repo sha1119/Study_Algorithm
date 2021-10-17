30b71f950379c36f817e98c6fdf184491d2f445d
sha11 <sha1119@naver.com>
2021-10-17 17:30:27 +0900

    No_2743 commit

---
+++ src/main/java/DataStructure/No_2743.java
@@ -0,0 +1,15 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = bufferedReader.readLine();
+
+        System.out.println(word.length());
+    }
+}
