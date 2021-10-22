4ea9c86ad02da3110cc5fcb48e89b0d0966ea83a
sha11 <sha1119@naver.com>
2021-10-23 08:29:58 +0900

    No_10718_re commit

---
+++ src/main/java/Step_1/No_10718.java
@@ -0,0 +1,10 @@
+package Step_1;
+
+public class No_10718 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("강한친구 대한육군").append('\n').append("강한친구 대한육군");
+        System.out.println(stringBuilder);
+    }
+}
